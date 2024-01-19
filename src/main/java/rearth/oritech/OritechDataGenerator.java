10f6199cfbc092b0df7e506534538cc06bf86544
rearth <davidus2010@gmail.com>
2024-01-19 20:41:50 +0100

    Initial commit

---
+++ src/main/java/rearth/oritech/OritechDataGenerator.java
@@ -0,0 +1,11 @@
+package rearth.oritech;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OritechDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
