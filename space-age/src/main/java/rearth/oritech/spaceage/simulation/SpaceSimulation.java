db234fa8c0566259474f2d8e81fb05a4321f1739
rearth <davidus2010@gmail.com>
2026-08-24 21:25:10 +0200

    Begin space simulation

---
+++ space-age/src/main/java/rearth/oritech/spaceage/simulation/SpaceSimulation.java
@@ -0,0 +1,121 @@
+package rearth.oritech.spaceage.simulation;
+
+import org.joml.Vector2f;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+
+// purely a data container.
+// one or more players can use the same space simulation
+// A player by default gets his own simulation. However if he uses a space interaction block / module that is already
+// used with another simulation, he joins that one.
+// There is also the option of manually leaving existing ones to get a new instance.
+
+// Positions in space are in 2d, where X is the "right" / relative offset to the height / orbit. Units are blocks
+// Y is "away" from the earth (distance from surface), with outer space and the sun both away from earth, but at different X positions.
+// different orbits are also at specific Y heights each.
+
+// general orbit bands / distances:
+// low earth orbit: ~1000 distance. High gravity
+// medium earth orbit: ~20 000 distance. Medium Gravity
+// High / Geostationary Orbit: ~40 000. Low gravity
+// initial outer space: ~100 000. Gravity influence reaches 0 at 100 000.
+// first few asteroids are between ~100000 and ~200000
+// sun: 3 000 000
+// mars: 8 000 000
+// asteroid ring: 20 000 000
+
+public class SpaceSimulation {
+
+    // this is always the same and initialized once:
+    private static final Set<SimulatedObject> celestialObjects = new HashSet<>();
+
+    private final Set<SimulatedObject> nonCelestialObjects = new HashSet<>();
+    private final UUID simulationId;
+
+    public SpaceSimulation(UUID loadedSimulationId, Set<SimulatedObject> loadedObjects) {
+        this.simulationId = loadedSimulationId;
+    }
+
+    public SpaceSimulation() {
+        this.simulationId = UUID.randomUUID();
+        generateRandomObjects();
+    }
+
+    private void generateRandomObjects() {
+        // add asteroids (and more things in the future) to nonCelestialObjects
+
+        var nearAsteroidCount = 5; // in range 100k - 200k
+        var mediumAsteroidCount = 20; // in range 1M - 18 M
+        var beltAsteroidCount = 20;    // in range 19.5M - 20.5M
+
+        for (var i = 0; i < nearAsteroidCount; i++) {
+            var asteroid = new Asteroid();
+            asteroid.currentPosition = new Vector2f(
+                    (float) (Math.random() * 200_000 - 100_000),
+                    (float) (Math.random() * 100_000 + 100_000)
+            );
+            asteroid.weight = (float) (Math.random() * 99 + 1);
+            nonCelestialObjects.add(asteroid);
+        }
+
+        for (var i = 0; i < mediumAsteroidCount; i++) {
+            var asteroid = new Asteroid();
+            asteroid.currentPosition = new Vector2f(
+                    (float) (Math.random() * 2_000_000 - 1_000_000),
+                    (float) (Math.random() * 17_000_000 + 1_000_000)
+            );
+            asteroid.weight = (float) (Math.random() * 99 + 1);
+            nonCelestialObjects.add(asteroid);
+        }
+
+        for (var i = 0; i < beltAsteroidCount; i++) {
+            var asteroid = new Asteroid();
+            asteroid.currentPosition = new Vector2f(
+                    (float) (Math.random() * 39_000_000 - 19_500_000),
+                    (float) (Math.random() * 1_000_000 + 19_500_000)
+            );
+            asteroid.weight = (float) (Math.random() * 99 + 1);
+            nonCelestialObjects.add(asteroid);
+        }
+    }
+
+    // initializes celestial Objects once
+    static {
+
+        var earth = new SimulatedObject();
+        earth.currentPosition = new Vector2f(0, 0);
+        celestialObjects.add(earth);
+
+        var sun = new SimulatedObject();
+        sun.currentPosition = new Vector2f(-1_000_000, 3_000_000);
+        celestialObjects.add(sun);
+
+        var mars = new SimulatedObject();
+        mars.currentPosition = new Vector2f(1_000_000, 8_000_000);
+        celestialObjects.add(mars);
+    }
+
+    // returns a value between 0 and 1
+    public static float getGravityStrength(float height) {
+        return Math.clamp(1 - height / 100_000, 0, 1);
+    }
+
+    public static class SimulatedObject {
+
+        public Vector2f currentPosition;
+
+    }
+
+    // Things like asteroids. Movement itsn't applied per tick, instead events / future positions are calculated during each interaction / event
+    public static class MovableSimulatedObject extends SimulatedObject {
+        public float weight;
+    }
+
+    public static class Asteroid extends MovableSimulatedObject {
+        // loot table data here, etc. in the future
+    }
+
+}
